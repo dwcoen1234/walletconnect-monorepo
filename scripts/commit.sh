bcf99bae51e44e68936c6ca31bc8fb950a98c528
Gancho Radkov <43912948+ganchoradkov@users.noreply.github.com>
2025-05-01 22:22:56 +0400

    Revert "chore: auto update lerna.json & relayer version"

--- scripts/commit.sh
+++
@@ -1,9 +0,0 @@
-# this script is used to commit version updates after changeset version including lerna.json & relayer version
-echo "Staging changes..."
-git add .
-
-# Commit the changes
-echo "Committing the changes..."
-git commit -m "chore: update versions and lerna.json"
-
-echo "✅ Version updates and lerna.json changes committed."
