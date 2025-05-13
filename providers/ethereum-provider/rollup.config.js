ab74ef97935c3ce78f47b18216b7c9191e92849b
Gancho Radkov <ganchoradkov93@gmail.com>
2025-05-13 17:16:44 +0300

    refactor: rm package and leave on `@reown/appkit/core` as excluded from rollup

--- providers/ethereum-provider/rollup.config.js
+++ providers/ethereum-provider/rollup.config.js
@@ -3,10 +3,7 @@ import createConfig from "../../rollup.config";
 // @walletconnect/modal has dynamic imports, so we need to enable inlineDynamicImports
 export default createConfig(
   name,
-  Object.keys({ ...dependencies, ...peerDependencies }).concat([
-    "@reown/appkit",
-    "@reown/appkit/core",
-  ]),
+  Object.keys({ ...dependencies, ...peerDependencies }).concat(["@reown/appkit/core"]),
   { inlineDynamicImports: true },
   { inlineDynamicImports: true },
   { inlineDynamicImports: true },
