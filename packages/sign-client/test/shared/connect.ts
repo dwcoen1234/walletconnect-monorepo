7efd4ccfe467b94f8334cc758419f3ecde52bb41
Gancho Radkov <ganchoradkov93@gmail.com>
2025-05-27 15:03:28 +0300

    fix(tests): requiredNamespaces checks

--- packages/sign-client/test/shared/connect.ts
+++ packages/sign-client/test/shared/connect.ts
@@ -50,7 +50,7 @@ export async function testConnectMethod(clients: Clients, params?: TestConnectPa
   const resolveSessionProposal = new Promise<void>((resolve, reject) => {
     B.once("session_proposal", async (proposal) => {
       try {
-        // expect(proposal.params.requiredNamespaces).to.eql(connectParams.requiredNamespaces);
+        expect(proposal.params.requiredNamespaces).to.eql({});
         expect(proposal.params.optionalNamespaces).to.eql(connectParams.optionalNamespaces);
         expect(proposal.params.sessionProperties).to.eql(TEST_SESSION_PROPERTIES);
         const { acknowledged } = await B.approve({
