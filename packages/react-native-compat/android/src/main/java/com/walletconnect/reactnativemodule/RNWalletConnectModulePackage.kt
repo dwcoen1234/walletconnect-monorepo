3db8b0ed5b91d9403e6742734300d8ffd8be142b
nacho <25931366+ignaciosantise@users.noreply.github.com>
2025-07-01 12:23:34 -0300

    chore: reverted turbomodule import change

--- packages/react-native-compat/android/src/main/java/com/walletconnect/reactnativemodule/RNWalletConnectModulePackage.kt
+++ packages/react-native-compat/android/src/main/java/com/walletconnect/reactnativemodule/RNWalletConnectModulePackage.kt
@@ -1,17 +1,13 @@
 package com.walletconnect.reactnativemodule
 
-import com.facebook.react.BaseReactPackage
+import com.facebook.react.TurboReactPackage
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.module.model.ReactModuleInfoProvider
 import com.facebook.react.module.model.ReactModuleInfo
 import java.util.HashMap
 
-class RNWalletConnectModulePackage : BaseReactPackage() {
-  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-    return listOf(RNWalletConnectModuleModule(reactContext))
-  }
-
+class RNWalletConnectModulePackage : TurboReactPackage() {
   override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
     return if (name == RNWalletConnectModuleModule.NAME) {
       RNWalletConnectModuleModule(reactContext)
@@ -23,13 +19,15 @@ class RNWalletConnectModulePackage : BaseReactPackage() {
   override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
     return ReactModuleInfoProvider {
       val moduleInfos: MutableMap<String, ReactModuleInfo> = HashMap()
+      val isTurboModule: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
       moduleInfos[RNWalletConnectModuleModule.NAME] = ReactModuleInfo(
         RNWalletConnectModuleModule.NAME,
         RNWalletConnectModuleModule.NAME,
         false,  // canOverrideExistingModule
         false,  // needsEagerInit
         true,  // hasConstants
-        false   // isCxxModule
+        false,   // isCxxModule,
+        isTurboModule // isTurboModule
       )
       moduleInfos
     }
