7a2b3aa80d63461e203c23564225dc72d1569e4d
Gancho Radkov <ganchoradkov93@gmail.com>
2025-07-15 15:25:26 +0300

    chore: uses prod relay

--- packages/core/src/constants/relayer.ts
+++ packages/core/src/constants/relayer.ts
@@ -2,7 +2,7 @@ export const RELAYER_DEFAULT_PROTOCOL = "irn";
 
 export const RELAYER_DEFAULT_LOGGER = "error";
 
-export const RELAYER_DEFAULT_RELAY_URL = "wss://staging-relay.walletconnect.com";
+export const RELAYER_DEFAULT_RELAY_URL = "wss://relay.walletconnect.org";
 
 export const RELAYER_CONTEXT = "relayer";
 
