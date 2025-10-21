bfcd5a011e52fb738724ed397b38b620a79f0908
Gancho Radkov <ganchoradkov93@gmail.com>
2025-10-21 11:12:08 +0300

    chore: rm `.only` modifier

--- packages/sign-client/test/sdk/wallet-pay.spec.ts
+++ packages/sign-client/test/sdk/wallet-pay.spec.ts
@@ -13,7 +13,7 @@ describe("Authenticated Sessions", () => {
     cryptoWallet = ethers.Wallet.createRandom();
   });
 
-  it.only("should receive wallet pay request and respond with wallet pay result. Case 1", async () => {
+  it("should receive wallet pay request and respond with wallet pay result. Case 1", async () => {
     const dapp = await SignClient.init({ ...TEST_SIGN_CLIENT_OPTIONS, name: "dapp" });
     expect(dapp).to.be.exist;
     expect(dapp.metadata.redirect).to.exist;
